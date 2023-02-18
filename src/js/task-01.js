d737d529bb30690f4de8c00961505ed5f59400b9
ViktorKryvorotov <113522935+ViktorKryvorotov@users.noreply.github.com>
2023-02-18 21:47:54 +0200

    update task-1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,9 @@
 const numberItemEl = document.querySelectorAll('li.item');
 console.log('Number of categories:',numberItemEl.length);
 
-numberItemEl.forEach((element)=>{
-    console.log('Category:',element.firstElementChild.textContent);
+numberItemEl.forEach((element) => {
+    console.log('Category:', element.firstElementChild.textContent);
     console.log('Elements:', element.lastElementChild.children.length);
 }
-)
+);
 
