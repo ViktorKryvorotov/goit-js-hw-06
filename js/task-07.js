56ae119388c3619380b15fcffeed1e25c031578b
ViktorKryvorotov <113522935+ViktorKryvorotov@users.noreply.github.com>
2023-02-22 13:08:39 +0200

    fixed

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 
 const fontSizeControl = document.querySelector('#font-size-control');
 const textSpanEl = document.querySelector('#text');
-textSpanEl.style.fontSize = fontSizeControl.value + 'px'
+textSpanEl.style.fontSize = fontSizeControl.value + 'px';
 
 fontSizeControl.addEventListener("input", (event) => {
     textSpanEl.style.fontSize = event.currentTarget.value + 'px';
